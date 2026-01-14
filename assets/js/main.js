e50c4f20c56b598492a4b77f43193ac782cc3817
KANDA <mefan434161@yahoo.co.jp>
2026-01-14 20:16:59 +0900

    origin commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //定義
   const drawerIcon = document.querySelector('.p-drawer__icon');
   const drawer = document.querySelector('.p-drawer');
-  const drawerNavItem = document.querySelectorAll('.p-drawer__body a[href^="#"]');
+  const drawerNavItem = document.querySelectorAll(' a[href^="#"]');
   const headerHeight = document.querySelector('.p-header').offsetHeight;
   const breakpoint = 768;
   let isMenuOpen = false;
@@ -32,15 +32,15 @@ document.addEventListener("DOMContentLoaded", () => {
   //メニューを開くアニメーション
   const openMenu = () => {
     if (!drawer.classList.contains("js-show")) {
-      drawer.classList.add("js-show");
+      drawer.classList.add("is-active");
       drawerIcon.classList.add("js-show");
     }
   }
 
   //メニューを閉じるアニメーション
   const closeMenu = () => {
-    if (drawer.classList.contains("js-show")) {
-      drawer.classList.remove("js-show");
+    if (drawer.classList.contains("is-active")) {
+      drawer.classList.remove("is-active");
       drawerIcon.classList.remove("js-show");
       isMenuOpen = false;
     }
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //メニューの開閉動作
   const toggleMenu = () => {
-    if (!drawer.classList.contains("js-show")) {
+    if (!drawer.classList.contains("is-active")) {
       openMenu();
     } else {
       closeMenu();
@@ -61,16 +61,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const windowWidth = window.innerWidth;
     if (windowWidth > bp && isMenuOpenAtBreakpoint) {
       closeMenu();
-    } else if (windowWidth <= bp && drawer.classList.contains("js-show")) {
+    } else if (windowWidth <= bp && drawer.classList.contains("is-active")) {
       isMenuOpenAtBreakpoint = true;
     }
   };
 
   //メニュー外クリック処理
   const clickOuter = (event) => {
-    if (drawer.classList.contains("js-show") && !drawer.contains(event.target) && isMenuOpen) {
+    if (drawer.classList.contains("is-active") && !drawer.contains(event.target) && isMenuOpen) {
       closeMenu();
-    } else if (drawer.classList.contains("js-show") && !drawer.contains(event.target)) {
+    } else if (drawer.classList.contains("is-active") && !drawer.contains(event.target)) {
       isMenuOpen = true;
     }
   }
@@ -102,4 +102,330 @@ document.addEventListener("DOMContentLoaded", () => {
       linkScroll(targetItem);
     });
   });
+
+  
+    drawerSearchItem.addEventListener("click", event => {
+      event.preventDefault();
+      const targetItem = document.querySelector(drawerSearchItem.getAttribute("href"));
+      linkScroll(targetItem);
+    });
+  
+
+
+jQuery(function($) {
+
+  $('.c-tab__btn-item').on('click', function () {
+    $('.c-tab__btn-item').removeClass('is-active');
+    // tabの切り替え
+    $('.c-tab__btn-item').attr('aria-selected', 'false');
+    $(this).attr('aria-selected', 'true');
+    $(this).addClass('is-active');
+
+    // tab panelの切り替え
+    $('.c-tab__content').removeClass('is-active');
+    $('#' + $(this).attr("aria-controls")).addClass('is-active');
+
+    return false;
+  });
+
 });
+});
+
+
+//スクロールしたらヘッダーにクラスを付与
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.p-header');
+  var headerLogo = document.querySelector('.p-header__logo');
+  var drawer__btn = document.querySelector('.p-drawer__icon');
+  // スクロール位置を取得
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  // 少しスクロールしたらクラスをつける
+  if (scrollTop > 100) {
+    header.classList.add('js-active');
+    headerLogo.classList.add('js-active');
+    drawer__btn.classList.add('js-active');
+  } else {
+    header.classList.remove('js-active');
+    headerLogo.classList.remove('js-active');
+    drawer__btn.classList.remove('js-active');
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  setUpAccordion();
+});
+const summary = document.querySelectorAll('details')
+summary.forEach((element) => {
+  element.open = true;
+});
+
+
+const setUpAccordion = () => {
+  const details = document.querySelectorAll(".js-details");
+  const IS_OPENED_CLASS = "is-opened";
+
+  details.forEach((element) => {
+    const summary = element.querySelector(".js-summary");
+    const content = element.querySelector(".js-content");
+
+    summary.addEventListener("click", (event) => {
+      // デフォルトの挙動を無効化
+      event.preventDefault();
+
+      // is-openedクラスの有無で判定（detailsのopen属性の判定だと、アニメーション完了を待つ必要がありタイミング的に不安定になるため）
+      if (element.classList.contains(IS_OPENED_CLASS)) {
+        // アコーディオンを閉じるときの処理
+        // アイコン操作用クラスを切り替える(クラスを取り除く)
+        element.classList.toggle(IS_OPENED_CLASS);
+
+        // アニメーション実行
+        closingAnim(content, element).restart();
+      } else {
+        // アコーディオンを開くときの処理
+        // アイコン操作用クラスを切り替える(クラスを付与)
+        element.classList.toggle(IS_OPENED_CLASS);
+        // open属性を付与
+        element.setAttribute("open", "true");
+        // アニメーション実行
+        openingAnim(content).restart();
+      }
+    });
+  });
+}
+/**
+ * アコーディオンを閉じる時のアニメーション
+ */
+const closingAnim = (content, element) => gsap.to(content, {
+  height: 0,
+  opacity: 0,
+  duration: 0.4,
+  ease: "power3.out",
+  overwrite: true,
+  onComplete: () => {
+    // アニメーションの完了後にopen属性を取り除く
+    element.removeAttribute("open");
+  },
+});
+
+/**
+ * アコーディオンを開く時のアニメーション
+ */
+const openingAnim = (content) => gsap.fromTo(
+  content,
+  {
+    height: 0,
+    opacity: 0,
+  },
+  {
+    height: "auto",
+    opacity: 1,
+    duration: 0.4,
+    ease: "power3.out",
+    overwrite: true,
+  }
+);
+
+document.addEventListener("DOMContentLoaded", function () {
+  let toTopButton = document.querySelector(".p-header");
+
+  // スクロールイベントを監視してTo Topボタンを制御
+  window.addEventListener("scroll", function () {
+    // 現在のスクロール位置を取得
+    let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    // スクロール位置が一定値よりも大きい場合にTo Topボタンを表示、それ以外は非表示にする
+    if (scrollPosition > 100) {
+      toTopButton.classList.add("is-active");
+    } else {
+      toTopButton.classList.remove("is-active");
+    }
+  });
+
+
+});
+
+gsap.utils.toArray('.js-parallax').forEach(wrap => {
+  const y = wrap.getAttribute('data-y') || -200;
+  gsap.to(wrap, {
+    y: y,    
+    scrollTrigger: {
+      trigger: wrap,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: 0.9,
+    }
+  })
+});
+
+    // ナビゲーションが押されたらスクロール位置まで移動する
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+  links.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      const targetId = item.getAttribute("href");
+      const target = document.querySelector(targetId);
+      if (target) {
+        // headerの高さを取得 (実際のheaderのクラス名と差し替えてください)
+        const headerOffset = document.querySelector(".l-header").offsetHeight;
+        const elementPosition = target.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.scrollY - headerOffset;
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
+
+
+    // 無限アニメーション
+document.addEventListener("DOMContentLoaded", () => {
+	const imagesScroll = document.querySelector("#js-images-scroll"); // 無限アニメーション対象の要素を取得
+	const images = document.querySelector("#js-images"); // 無限アニメーションの1週分の要素を取得
+	const imagesWidth = images.scrollWidth; // 幅を取得
+	console.log('imagesWidth:', imagesWidth);
+
+	// itemsを複製
+	const imagesClone = images.cloneNode(true); // imagesを複製
+	imagesScroll.appendChild(imagesClone); // 複製した要素をimagesScrollに追加	
+	imagesClone.classList.add("is-clone"); // is-cloneクラスを追加
+
+	imagesScroll.style.setProperty('--images-width', `${imagesWidth}px`); // CSS変数にセット
+});
+
+  //キャッチコピーの出現アニメーション
+document.addEventListener("DOMContentLoaded", () => {
+  const mainImage = document.querySelector(".l-mv__scroll-animation");
+  const mainText = document.querySelectorAll(".section__title-en");
+
+  mainImage.classList.add('is-active');
+  
+  mainText.forEach((element, index) => {
+    element.style.animationDelay = `${index * 0.2}s`;
+  
+    setTimeout(() => {
+      element.classList.add('is-active');
+    }, 500);
+  });
+  
+});
+
+window.addEventListener('load',function(){
+  gsap.registerPlugin(ScrollTrigger);
+ 
+   //見出しの文字が下から出現
+   const underTextItems = document.querySelectorAll('.c-heading__lv2');
+   underTextItems.forEach(item => {
+     ScrollTrigger.create({
+       trigger: item,
+       start: "top 70%", // 要素が上部から70%の位置で発火
+       onEnter: () => {
+         // 要素内に入ったら、js-showクラスをつける
+         item.classList.add("is-active");
+       }
+     });
+   });
+
+     //見出しの文字が下から出現
+     const openDoorImage = document.querySelector('.l-cta__container');
+     
+       ScrollTrigger.create({
+         trigger: openDoorImage,
+         start: "top 70%", // 要素が上部から70%の位置で発火
+         end: "top 70%",
+         onEnter: () => {
+           // 要素内に入ったら、js-showクラスをつける
+           openDoorImage.classList.add("is-active");
+         },
+         onLeaveBack: () => {
+          openDoorImage.classList.remove("is-active");
+          // スクロール位置が「end」を超えて前方にスクロールしたときに発火
+        },
+       });
+   
+ });
+
+ document.addEventListener('DOMContentLoaded', function() {
+  
+  const modal = document.querySelectorAll('.c-member__modal');
+  // ダイアログを開く
+  const open = document.querySelectorAll('.c-bl__modal-btn');
+  const bg = document.querySelector('.l-modal-bg');
+  const body = document.querySelector('body');
+  open.forEach(button => {
+    button.addEventListener('click', (target) => {
+      target.preventDefault();
+      const dialogId = button.getAttribute('data-modal');
+      const dialog = document.getElementById(dialogId);
+     
+
+      modal.forEach(item => {
+        bg.classList.add('is-active');
+        item.classList.remove('is-active');
+        body.style.overflowY = 'hidden';
+        if(item.id == dialogId) {
+          item.classList.add('is-active');
+        }
+        
+      });
+     
+    });
+
+      // ダイアログを閉じる
+  const close = document.querySelectorAll('.c-member__modal-block');
+  close.forEach(button => {
+    button.addEventListener('click', () => {
+      bg.classList.remove('is-active');
+      body.style.overflowY = 'auto';
+      modal.forEach(item => {
+        item.classList.remove('is-active');
+      });
+    });
+  });
+  });
+
+     // ダイアログを閉じる
+   bg.addEventListener('click', () => {
+      bg.classList.remove('is-active');
+      body.style.overflowY = 'auto';
+      modal.forEach(item => {
+        item.classList.remove('is-active');
+      });
+    });
+  });
+
+  //  // オーバーレイクリックで変化させる
+  // document.addEventListener('DOMContentLoaded', function() {
+  
+  //    const card_button = document.querySelectorAll('.c-bl__btn');
+  //     card_button.forEach(button => {
+      
+  //     button.addEventListener('click', (target) => {
+  //       card_button.forEach(button => {
+  //         button.classList.remove('is-active');
+  //       });
+  //       target.preventDefault();
+  //       button.classList.add('is-active');
+  //     })
+  //   });
+  // });
+
+
+
+
+  // オーバーレイクリックでダイアログを閉じる
+  // dialogs.forEach(dialog => {
+  //   dialog.addEventListener('click', (event) => {
+  //     if (event.target.closest('.modal__inner') === null) {
+  //       dialog.classList.remove('js-show');
+  //       dialog.close();
+  //     }
+  //   });
+  // });
+
+  
+
+  
+
---
+++ assets/js/swiper.js
@@ -0,0 +1,191 @@
+const member_swipe = new Swiper('.member__swiper', { //swiperの名前
+  //切り替えのモーション
+  speed: 1000, //表示切り替えのスピード
+  effect: "slide", //切り替えのmotion (※1)
+  allowTouchMove: true, // スワイプで表示の切り替えを有効に
+
+  //最後→最初に戻るループ再生を有効に
+  loop: true,
+  //自動スライドについて
+  // autoplay: { 
+  //   delay: 3000, //何秒ごとにスライドを動かすか
+  //   stopOnLastSlide: false, //最後のスライドで自動再生を終了させるか
+  //   disableOnInteraction: true, //ユーザーの操作時に止める
+  //   reverseDirection: false, //自動再生を逆向きにする
+  // },
+
+   //表示について
+  centeredSlides: false, //中央寄せにする
+  slidesPerView: "1.2",
+  spaceBetween: 20,
+     // スワイパーのバーの動きのカスタマイズ
+
+
+  on: {
+    slideChange() {
+      const bar = document.querySelector('.progress__bar');
+      const total = this.slides.length - this.loopedSlides * 2 || this.slides.length;
+      const index = this.realIndex;
+      const maxMove = 750; // progress幅 - bar幅
+
+      const x = (maxMove / (total - 1)) * index;
+
+      bar.style.transform = `translateX(${x}px)`;
+    },
+  },
+
+
+  //ページネーション
+  pagination: {
+    el: ".swiper-pagination", //paginationのclass
+    clickable: true, //クリックでの切り替えを有効に
+    type: "progressbar" //paginationのタイプ (※2)
+  },
+  //ナビゲーション
+  navigation: {
+    prevEl: ".swiper-button-prev", //戻るボタンのclass
+    nextEl: ".swiper-button-next" //進むボタンのclass
+  },
+
+
+  //ブレイクポイントによって変える
+  breakpoints: { 
+    768: {
+      slidesPerView: 3.2,
+      spaceBetween: 40,
+    },
+    
+  }
+});
+
+/* =================================================== 
+※1 effectについて
+slide：左から次のスライドが流れてくる
+fade：次のスライドがふわっと表示
+■ fadeの場合は下記を記述
+  fadeEffect: {
+    crossFade: true
+  },
+cube：スライドが立方体になり、3D回転を繰り返す
+coverflow：写真やアルバムジャケットをめくるようなアニメーション
+flip：平面が回転するようなアニメーション
+cards：カードを順番にみていくようなアニメーション
+creative：カスタマイズしたアニメーションを使うときに使用します
+
+=======================================================
+※2 paginationのタイプ
+bullets：スライド枚数と同じ数のドットが表示
+fraction：分数で表示（例：1 / 3）
+progressbar：スライドの進捗に応じてプログレスバーが伸びる
+custom：自由にカスタマイズ
+
+=====================================================*/
+
+const cardSwiper = new Swiper('.card__swiper', { //swiperの名前
+  //切り替えのモーション
+  speed: 1000, //表示切り替えのスピード
+  effect: "slide", //切り替えのmotion (※1)
+  allowTouchMove: true, // スワイプで表示の切り替えを有効に
+
+  //最後→最初に戻るループ再生を有効に
+  loop: true,
+  //自動スライドについて
+  // autoplay: { 
+  //   delay: 3000, //何秒ごとにスライドを動かすか
+  //   stopOnLastSlide: false, //最後のスライドで自動再生を終了させるか
+  //   disableOnInteraction: true, //ユーザーの操作時に止める
+  //   reverseDirection: false, //自動再生を逆向きにする
+  // },
+
+   //表示について
+  centeredSlides: false, //中央寄せにする
+  slidesPerView: "1.1",
+  spaceBetween: 12,
+
+  // //ページネーション
+  // pagination: {
+  //   el: ".swiper-pagination", //paginationのclass
+  //   clickable: true, //クリックでの切り替えを有効に
+  //   type: "bullets" //paginationのタイプ (※2)
+  // },
+  // //ナビゲーション
+  // navigation: {
+  //   prevEl: ".swiper-button-prev", //戻るボタンのclass
+  //   nextEl: ".swiper-button-next" //進むボタンのclass
+  // },
+  // //スクロールバー
+  // scrollbar: { //スクロールバーを表示したいとき
+  //   el: ".swiper-scrollbar", //スクロールバーのclass
+  //   hide: true, //操作時のときのみ表示
+  //   draggable: true //スクロールバーを直接表示できるようにする
+  // },
+
+  //ブレイクポイントによって変える
+  breakpoints: { 
+    520: {
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+    },
+    920: {
+      slidesPerView: 1.5,
+      spaceBetween: 24,
+    },
+    1300: {
+      slidesPerView: 2.1,
+      spaceBetween: 24,
+    },
+    1500: {
+      slidesPerView: 2.6,
+      spaceBetween: 24,
+    },
+  }
+});
+
+// swiperのスライド変更時に実行されるイベントリスナー
+cardSwiper.on('slideChange', function() {
+  const activeIndex = cardSwiper.realIndex + 1; // 現在のスライドのインデックス+1を取得
+  const items = document.querySelectorAll('.c-bl__btn');
+
+  items.forEach(item => {
+    // すべてのアイテムからアクティブクラスを削除
+    item.classList.remove('is-active');
+  });
+
+  // 現在のスライドに対応するアイテムにアクティブクラスを追加
+  const currentItem = document.querySelector(`.c-bl__btn[data-slide="${activeIndex}"]`);
+  if (currentItem) {
+    currentItem.classList.add('is-active');
+  }
+});
+
+// card__list-item をクリックしたときに対応するスライドに切り替え
+const items = document.querySelectorAll('.c-bl__btn');
+items.forEach(item => {
+  item.addEventListener('click', function() {
+    const slideIndex = parseInt(item.getAttribute('data-slide'), 10) - 1; // data-slideは1始まりなので、0始まりに合わせる
+    cardSwiper.slideToLoop(slideIndex, 1000); // 該当スライドに切り替える
+  });
+});
+
+/* =================================================== 
+※1 effectについて
+slide：左から次のスライドが流れてくる
+fade：次のスライドがふわっと表示
+■ fadeの場合は下記を記述
+  fadeEffect: {
+    crossFade: true
+  },
+cube：スライドが立方体になり、3D回転を繰り返す
+coverflow：写真やアルバムジャケットをめくるようなアニメーション
+flip：平面が回転するようなアニメーション
+cards：カードを順番にみていくようなアニメーション
+creative：カスタマイズしたアニメーションを使うときに使用します
+
+=======================================================
+※2 paginationのタイプ
+bullets：スライド枚数と同じ数のドットが表示
+fraction：分数で表示（例：1 / 3）
+progressbar：スライドの進捗に応じてプログレスバーが伸びる
+custom：自由にカスタマイズ
+
+=====================================================*/
